4933c2d8c94b885837514e6f75f9a25c6e0078bf
asaf.1yon1 <asaf.yairbarish1@gmail.com>
2025-04-03 14:41:25 +0300

    add 03App

--- 3.4_fanction/03App.js
+++ 3.4_fanction/03App.js
@@ -1,17 +1,44 @@
-const isEven = a => {
-    if (a % 2 == 0) {
-        return true
-    }
-    return false
-}
+// const isEven = a => {
+//     if (a % 2 == 0) {
+//         return true
+//     }
+//     return false
+// }
 
-function comput(callback) {
-    let sol = callback(8)
-    console.log(sol);
+// function comput(fdfdewq , x) {
+//     let sol = fdfdewq(8)
+//     let sol1 = x(567)
+//     console.log(sol);
     
+// }
+
+// comput(isEven)
+// comput(isEven)
+
+
+// const sumlist  = Arr => {
+//     let sum = 0
+//     for (let i = 0; i < Arr.length; i++) {
+//         sum += Arr[i]
+//     }
+//     return sum
+// }
+
+
+function handleList(numbers, callback){
+    callback(numbers);
 }
 
-comput(isEven)
+const avgList = numbers => {
+    let sum = 0; 
+    for(let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    console.log(sum / numbers.length);
+}
+
+handleList([1, 5, 6], avgList);
+
 
 
 
